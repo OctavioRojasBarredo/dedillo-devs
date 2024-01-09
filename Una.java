96fd8fddd3318bb7a4b5fa88a869334fd56d2f62
Gabriel <owelcontacto@gmail.com>
2024-01-09 11:05:33 +0100

    Segundo commit dedil

--- Una.java
+++ Una.java
@@ -9,4 +9,20 @@ public class Una
         esLijada = false;
         longitudUna = 1;
     }
+    
+    public int getLongitudUna(){
+        return longitudUna;
+    }
+    
+    public boolean getLijado(){
+        return esLijada;
+    }
+    public void setLongitudUna(int nuevaLongitudUna){
+        longitudUna = nuevaLongitudUna;
+    }
+    
+    public void setLijado(){
+        esLijada = !esLijada;
+    }
+    
 }

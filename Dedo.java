52c1e12baaadee2c20d89b6c63c5320594041604
OctavioRojasBarredo <octaviorojasbarredo@gmail.com>
2024-01-23 10:58:20 +0100

    Séptimo commit dedil

--- Dedo.java
+++ Dedo.java
@@ -3,22 +3,36 @@ public class Dedo
     private String tipoDeDedo;
     private int longitudDedo;
     private boolean una;
- 
+
     public Dedo()
     {
         tipoDeDedo = "Normal";
         longitudDedo = 5;
         una = true;
     }
-    
+
     public String getTipoDeDedo(){
         return tipoDeDedo;
     }
+
     public int getLongitudDedo(){
         return longitudDedo;
     }
+
     public boolean getUna(){
         return una;
     }
+
+    public void setTipoDeDedo(String nuevoTipoDeDedo) {
+        tipoDeDedo = nuevoTipoDeDedo;
+    }
     
+    public void setLongitudDeDedo(int nuevaLongitud) {
+        longitudDedo = nuevaLongitud;
+    }
+
+    public void setTieneUna (boolean tieneUna) {
+        una = tieneUna;
+    }
+
 }
